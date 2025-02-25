48d3e3b965bbe5fea6e0d837fbd3858547fe32df
Jay Davis <jdlikesbsbl@gmail.com>
2025-02-25 12:11:15 -0600

    adds fetch to listingcontainer

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,28 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useState, useEffect } from "react";
+import ListingCard from "./ListingCard";
 
 function ListingsContainer() {
+  const [listings, setListings] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:6001")
+      .then(resp => resp.json())
+      .then(data => setListings(data))
+  }, [])
+
+  const listingData = listings.map((listing) => (
+    <ListingCard 
+      key={listing.id}
+      description={listing.description}
+      image={listing.image}
+      location={listing.location}
+    />
+  ))
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listingData}
       </ul>
     </main>
   );
